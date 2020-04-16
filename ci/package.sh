6d7c8e1f67995c8d70eaaff52edec87ced5da974
Brian Sullivan <bsulliv@us.ibm.com@Brians-MacBook-Pro.local>
2020-04-16 11:30:54 -0400

    output SHAs

--- ci/package.sh
+++ ci/package.sh
@@ -57,10 +57,14 @@ done
 # build archive of pipelines
 tar -czf $assets_dir/default-kabanero-pipelines.tar.gz -C $pipelines_dir .
 touch $assets_dir/default-kabanero-pipelines-tar-gz-sha256
-echo $(($sha256cmd $assets_dir/default-kabanero-pipelines.tar.gz) | awk '{print $1}') >> $assets_dir/default-kabanero-pipelines-tar-gz-sha256
+tektonSHA=$(($sha256cmd $assets_dir/default-kabanero-pipelines.tar.gz) | awk '{print $1}')
+echo ${tektonSHA}
+echo ${tektonSHA}>> $assets_dir/default-kabanero-pipelines-tar-gz-sha256
 tar -czf $assets_dir/eventing-kabanero-pipelines.tar.gz -C $eventing_pipelines_dir .
 touch $assets_dir/eventing-kabanero-pipelines-tar-gz-sha256
-echo $(($sha256cmd $assets_dir/eventing-kabanero-pipelines.tar.gz) | awk '{print $1}') >> $assets_dir/eventing-kabanero-pipelines-tar-gz-sha256
+eventSHA=$(($sha256cmd $assets_dir/eventing-kabanero-pipelines.tar.gz) | awk '{print $1}')
+echo ${eventSHA}
+echo ${eventSHA} >> $assets_dir/eventing-kabanero-pipelines-tar-gz-sha256
 echo -e "--- Created pipeline artifacts"
 # expose an extension point for running after main 'package' processing
 exec_hooks $script_dir/ext/post_package.d
