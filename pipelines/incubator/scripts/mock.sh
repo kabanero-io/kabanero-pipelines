1989bb8bb00b3bab3972a389d81ea0bedecd4186
Scott McClements <smcclem@us.ibm.com>
2020-04-29 12:37:27 -0400

    WIP

--- pipelines/incubator/scripts/mock.sh
+++ pipelines/incubator/scripts/mock.sh
@@ -2,11 +2,20 @@
 
 
 skopeo () {
-   echo OVERRIDE skopeo!
+  cat skopeo.txt
 }
 
 
 kubectl () {
-   echo OVERRIDE kubectl!
+   if [[ "$2" == "stack" ]]; then
+      cat kubectl_stack.txt
+      return
+   fi
+   if [[ "$2" == "kabanero" ]]; then
+      cat kubectl_kabanero.txt
+      return
+   fi
+   echo "ERROR: Unknown option"
+   exit 1
 }
 
