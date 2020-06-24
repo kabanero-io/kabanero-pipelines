044345cef22289b4e0962ab654584c9a0e7d5db6
Scott McClements <smcclem@us.ibm.com>
2020-06-24 15:22:20 -0400

    WIP

--- pipelines/tests/test-pipelines/01-test-default-pipeline.sh
+++ pipelines/tests/test-pipelines/01-test-default-pipeline.sh
@@ -24,13 +24,8 @@ git_url="https://github.com/smcclem/"
 docker_url="smcclem"
 collection=$( date +%s%N  )
 
-echo
-echo "Apply: "$collection
-echo
-
 cd /workspace/$gitsource/pipelines/incubator/events
 find . -type f -name '*.yaml' -exec sed -i "s/@Digest@/$collection/g" {} \;
-find . -type f -name '*.yaml' -exec cat  {} \;
 find . -type f -name '*.yaml' -exec kubectl apply -f  {} \;
 
 #./kabanero-pipelines/pipelines/incubator/manual-pipeline-runs/manual-pipeline-run-script.sh
